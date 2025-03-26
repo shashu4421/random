4a8a314c15567dad26de85ab34c4a99812277121
shashu4421 <shashankkdrolia@gmail.com>
2025-03-26 18:31:39 +0530

    Update Snippet.scala

--- Snippet.scala
+++ Snippet.scala
@@ -13,3 +13,5 @@ df_normalized.show(false)
 val dfWithDiffs = dfJoined.withColumn("Diffs", array(diffStructs: _*))
   .withColumn("Diffs", expr("filter(Diffs, d -> d.Old_Value != d.New_Value OR d.Old_Value IS NULL OR d.New_Value IS NULL AND NOT (d.Old_Value IS NULL AND d.New_Value IS NULL))"))
 
+val dfWithDiffs = dfJoined.withColumn("Diffs", array(diffStructs: _*))
+  .withColumn("Diffs", expr("filter(Diffs, d -> d.Old_Value != d.New_Value OR (d.Old_Value IS NULL AND d.New_Value IS NOT NULL) OR (d.Old_Value IS NOT NULL AND d.New_Value IS NULL))"))
