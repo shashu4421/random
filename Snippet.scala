bbcfa53571f2f1d594764a83dfc9101c9fd2d425
shashu4421 <shashankkdrolia@gmail.com>
2025-03-26 18:46:43 +0530

    Update Snippet.scala

--- Snippet.scala
+++ Snippet.scala
@@ -15,3 +15,25 @@ val dfWithDiffs = dfJoined.withColumn("Diffs", array(diffStructs: _*))
 
 val dfWithDiffs = dfJoined.withColumn("Diffs", array(diffStructs: _*))
   .withColumn("Diffs", expr("filter(Diffs, d -> d.Old_Value != d.New_Value OR (d.Old_Value IS NULL AND d.New_Value IS NOT NULL) OR (d.Old_Value IS NOT NULL AND d.New_Value IS NULL))"))
+
+
+import org.apache.spark.sql.functions._
+
+val dfNormalized = df.withColumn(
+  "UAT_Normalized",
+  when(col("Column") === "legDetails",
+       regexp_replace(col("UAT"), "(\\.0+)$", "") // Remove .000000 only for legDetails
+  ).otherwise(col("UAT"))
+).withColumn(
+  "Production_Normalized",
+  when(col("Column") === "legDetails",
+       regexp_replace(col("Production"), "(\\.0+)$", "") // Remove .000000 only for legDetails
+  ).otherwise(col("Production"))
+)
+
+// Compare after normalization
+val dfFinal = dfNormalized.withColumn("isEqual",
+  col("Production_Normalized") === col("UAT_Normalized")
+)
+
+dfFinal.show(false)
