89be1309478d0b40e2745bc92bd82963911c0e9d
shashu4421 <shashankkdrolia@gmail.com>
2025-04-10 16:26:58 +0530

    Update ok.scala

--- ok.scala
+++ ok.scala
@@ -10,3 +10,22 @@ val schemaMap: Map[String, List[(String, String)]] = dfA
     val nameTypeList = parsed.select($"name", $"jsonDatatype").as[(String, String)].collect().toList
     (eventName, nameTypeList)
   }.toMap
+
+
+
+// Step 2: Define schema of fields column (which is a JSON array of structs)
+val fieldArraySchema = ArrayType(StructType(Seq(
+  StructField("name", StringType),
+  StructField("jsonDatatype", StringType)
+)))
+
+// Step 3: Parse the JSON string column "fields" to Array of Structs
+val dfParsed = df.withColumn("parsedFields", from_json(col("fields"), fieldArraySchema))
+
+// Step 4: Extract just the "name" from each field
+val dfFinal = dfParsed
+  .withColumn("fields", expr("transform(parsedFields, x -> x.name)"))
+  .select("eventName", "fields")
+
+// Show result
+dfFinal.show(false)
