a3a4a5934d080f2a1c1af3ad5a39cef613127e64
shashu4421 <shashankkdrolia@gmail.com>
2025-04-16 16:57:41 +0530

    Update diff_updates.scala

--- diff_updates.scala
+++ diff_updates.scala
@@ -24,3 +24,28 @@ def generateDiff(prodDF: DataFrame, devDF: DataFrame, eventType: String): DataFr
 
   diff
 }
+
+
+
+import org.apache.spark.sql.functions._
+import uk.co.gresearch.spark.diff._
+
+def generateDiff(prodDF: DataFrame, devDF: DataFrame, eventType: String): DataFrame = {
+  val keyCols = if (eventType == "MEOT" || eventType == "MEOF")
+    Seq("tradeID", "tradeKeyDate")
+  else
+    Seq("orderID", "orderKeyDate")
+
+  val compareCols = Seq("firmROEID", "CATReporterIMID", "eventTimestamp")
+
+  val diff = prodDF.diff(devDF, keyCols, compareCols).filter(col("diff") =!= lit("N"))
+
+  val changedColsExprs = compareCols.map { c =>
+    when(col(s"${c}_diff").isNotNull, lit(c)).otherwise(null)
+  }
+
+  val withChangedCols = diff.withColumn("changedColumns", expr(s"filter(array(${compareCols.map(c => s"IF(${c}_diff IS NOT NULL, '$c', NULL)").mkString(", ")}), x -> x IS NOT NULL)"))
+
+  withChangedCols
+}
+
